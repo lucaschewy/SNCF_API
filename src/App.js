c34ad3bb8b6d7c475d039d6be20f45ffd2d3e297
Lucas Chevalier <lucaschevalier@MacBook-Pro-de-Lucas.local>
2020-03-10 11:25:40 +0100

    premier test api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+    error: null,
+    isLoaded: false,
+    items: []
+  };
+
+  componentDidMount() {
+    fetch("https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area%3AOCE%3ASA%3A87581009/departures?calendar=20200314T000000&key=28788241-cfd6-480f-baa3-aa071e03a3c5")
+      .then(res => res.json())
+      .then((result) => {
+          console.log(result)
+          let items = result.departures
+          console.log(items)
+          this.setState({
+            isLoaded: true,
+            items
+          });
+        },
+
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Chargement…</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.display_informations.direction}>{item.display_informations.direction}</li>
+          ))}
+        </ul>
+      );
+    }
+  }
 }
 
 export default App;
